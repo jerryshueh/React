77934539e831f74ca1212c204b8da2147678c32f
Alexey Polioviy <WindowsMac@Alexeys-Mac.local>
2017-08-08 02:40:34 -0400

    react-router, react-bootstrap added

---
+++ src/App.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {LinkContainer} from 'react-router-bootstrap'
+import {Navbar, Nav, NavItem, FormGroup, ControlLabel, FormControl, HelpBlock, form, Jumbotron, ProgressBar} from 'react-bootstrap'
+import styles from './style.css';
+
+const App = () => 
+(
+  <Router>
+    <div>
+      <Route path="/" render={() => <Header /> } />
+      <Route exact path="/" render={() => <Home /> } />
+      <Route path="/about" render={() => <About />} />
+      <Route path="/contact" children={({match}) => match && <Contact />} />
+      <Route path="/" render={() => <Footer /> } />
+    </div>
+  </Router>
+)
+
+const Header = () => 
+(
+  <div>    
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+        <LinkContainer to="/">
+            <NavItem eventKey={1}>Home</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/about">
+            <NavItem eventKey={2}>About</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/contact">
+            <NavItem eventKey={3}>Contact</NavItem>
+          </LinkContainer>
+        </Nav>
+        <Nav >
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
+)
+
+const Footer = () => 
+(
+  <footer className="footer">     
+    <Navbar inverse collapseOnSelect>
+      
+    </Navbar>
+  </footer>
+)
+
+const About = () =>
+(
+  <div>
+  <Jumbotron>
+  <h1>Active Progress Bars </h1>
+    <ProgressBar bsStyle="success" active now={40} />
+    <ProgressBar bsStyle="info" active now={20} />
+    <ProgressBar bsStyle="warning" active now={60} />
+    <ProgressBar bsStyle="danger" active now={80} />
+  </Jumbotron>
+  
+  </div>
+)
+
+const Home = () =>
+(
+ <div>
+  <Jumbotron>
+  <h1>Stacked Progress Bar </h1>
+  <ProgressBar>
+    <ProgressBar striped bsStyle="success" now={35} key={1} />
+    <ProgressBar bsStyle="warning" now={20} key={2} />
+    <ProgressBar active bsStyle="danger" now={10} key={3} />
+  </ProgressBar>
+  </Jumbotron>
+  </div>
+)
+
+const Contact = () =>
+(
+  <div>
+  <Jumbotron>
+    <FormExample />
+  </Jumbotron>
+  <Jumbotron>
+    <FormExample />
+  </Jumbotron>
+  <Jumbotron>
+    <FormExample />
+  </Jumbotron>
+  </div>
+
+)
+// form example
+const FormExample = React.createClass({
+  getInitialState() {
+    return {
+      value: ''
+    };
+  },
+
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+  },
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  },
+
+  render() {
+    return (
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Form validation</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
+        </FormGroup>
+      </form>
+    );
+  }
+});
+
+
+
+export default App
